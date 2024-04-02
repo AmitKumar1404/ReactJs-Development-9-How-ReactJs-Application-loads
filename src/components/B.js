8ab84c0b7c4a76765c370c3eb274124964d632af
Amit Kumar <amitkumr1404@gmail.com>
2024-04-03 01:59:49 +0530

    commit 2

---
+++ src/components/B.js
@@ -0,0 +1,26 @@
+// 1. Import Area
+import React, { Component } from "react"
+import C from './C';
+// 2. Definition Area
+//  Lets define another component
+export class B extends Component
+{
+  // 1. Properties
+
+  // 2. Constructor
+
+  // 3. Method
+  // Every class component must have render method
+  render()
+  {
+    return (
+        <React.Fragment>
+            <div>MICROSOFT {this.props.data3}</div>
+            <C data4={this.props.data3}></C>
+        </React.Fragment>
+    );
+  }
+}
+
+
+// 3. Export Area
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1. import {someNameImport} from somelocation/somelibrary
+// 2. import someDefaultImport from somelocation/somelibrary
+import A from './components/A';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('amit'));
+root.render(<A name="Virat" surname="Kohli">Data2</A>);
